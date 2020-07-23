6ef8a4a04fadee526e7cec5c53bfb3a138747ffb
Maria Claudia <mariaclaudia.correia@yahoo.com.br>
2020-07-23 10:06:59 -0300

    adicionando informações

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function () {
+     $(document).scroll(function(){
+          if(window.scrollY > 200){
+          console.log('oi')
+$('#imgmulher').fadeIn('slow');
+          }
+      })
+
+
+});
+   
+// $("#myimage").hide();
+// $("#mybutton").click(function(){ 
+//      //Parâmetro slow: o efeito é executado em 600ms
+//      $("myimage").fadeIn("slow");
+// })
+    
+     
